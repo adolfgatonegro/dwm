68092e6a5508040675a3750257740a32f2f539ac
adolfgatonegro <github@gatoneg.ro>
2024-08-23 16:05:30 -0600

    reorder layouts, add bindings for most used

--- config.h
+++ config.h
@@ -12,10 +12,10 @@
 static const unsigned int borderpx       = 1;   /* border pixel of windows */
 static const unsigned int snap           = 32;  /* snap pixel */
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
-static const unsigned int gappih         = 8;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 8;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 8;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 8;  /* vert outer gap between windows and screen edge */
+static const unsigned int gappih         = 4;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 4;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 4;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 4;  /* vert outer gap between windows and screen edge */
 static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
@@ -27,7 +27,7 @@ static const unsigned int systrayiconsize = 16; /* systray icon size in px, in b
 static const int showsystray             = 1;   /* 0 means no systray */
 
 /* Indicators: see patch/bar_indicators.h for options */
-static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
+static int tagindicatortype              = INDICATOR_TOP_BAR;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
 static const char *fonts[]               = { "monospace:size=8" };
@@ -236,12 +236,12 @@ static const int lockfullscreen = 1;   /* 1 will force focus on the fullscreen w
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
-	{ "HHH",      grid },
 	{ "[D]",      deck },
-	{ "TTT",      bstack },
-	/* { "|M|",      centeredmaster }, */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "|M|",      centeredmaster },
+	{ "HHH",      grid },
+	/* { "TTT",      bstack }, */
+	{ "[M]",      monocle },
+	/* { "><>",      NULL },    /1* no layout function means floating behavior *1/ */
 };
 
 /* key definitions */
@@ -288,7 +288,6 @@ static const Key keys[] = {
 	{ Mod1Mask,                     XK_o,         spawn,       {.v = (const char*[]){ "linkhandler", NULL } } },
 	{ Mod1Mask|ControlMask,         XK_v,         spawn,       {.v = (const char*[]){ "vim-anywhere", NULL } } },
 	{ MODKEY,                       XK_F12,       spawn,       {.v = (const char*[]){ "simplescreenrecorder", "--start-hidden", NULL } } },
-	/* { MODKEY|ShiftMask,             XK_F12,       spawn,       {.v = (const char*[]){ "dmrecord", "kill", NULL } } }, */
 	{ MODKEY|ControlMask,           XK_p,         spawn,       {.v = (const char*[]){ "dmdisplay", NULL } } },
 	{ MODKEY,                       XK_b,         spawn,       {.v = (const char*[]){ BROWSER, NULL } } },
 	{ MODKEY|ShiftMask,             XK_b,         spawn,       {.v = (const char*[]){ "dmbrowser", NULL } } },
@@ -329,8 +328,10 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask,           XK_comma,      focusmon,               {.i = -2 } },
 	{ MODKEY|ControlMask,           XK_period,     focusmon,               {.i = +1 } },
 	/* layouts */
-	{ Mod1Mask|ControlMask,         XK_t,          setlayout,              {.v = &layouts[0]} },
-	{ Mod1Mask|ControlMask,         XK_f,          setlayout,              {.v = &layouts[1]} },
+	{ Mod1Mask,                     XK_t,          setlayout,              {.v = &layouts[0]} },
+	{ Mod1Mask,                     XK_d,          setlayout,              {.v = &layouts[1]} },
+	{ Mod1Mask,                     XK_g,          setlayout,              {.v = &layouts[3]} },
+	{ Mod1Mask,                     XK_m,          setlayout,              {.v = &layouts[5]} },
 	/* gaps */
 	{ MODKEY|ControlMask,           XK_0,          togglegaps,             {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_0,          defaultgaps,            {0} },
