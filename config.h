f0a70219d4d907494dbb3133652a8c372edf8536
adolfgatonegro <github@gatoneg.ro>
2024-10-03 21:34:53 -0600

    change font: cozette

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ static const int showsystray             = 1;   /* 0 means no systray */
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
-static const char *fonts[]               = { "monospace:size=8" };
+static const char *fonts[]               = { "Cozette:size=8" };
 /* static const char dmenufont[]            = "monospace:size=9"; */
 
 static char c000000[]                    = "#000000"; // placeholder value
