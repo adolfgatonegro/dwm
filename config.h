cec72f66640937e23b336f4b99ea645d98376b84
adolfgatonegro <github@gatoneg.ro>
2024-10-06 15:44:12 -0600

    set smartgap factor to 0 for single windows

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const unsigned int gappih         = 4;  /* horiz inner gap between window
 static const unsigned int gappiv         = 4;  /* vert inner gap between windows */
 static const unsigned int gappoh         = 4;  /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 4;  /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
 static const int bar_height              = 25;   /* 0 means derive from font, >= 1 explicit height */
