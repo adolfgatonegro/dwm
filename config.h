045df741aa9262ab00186cbfeab53be562ebef6c
adolfgatonegro <github@gatoneg.ro>
2024-10-15 23:25:19 -0600

    disable some inplacerotate keys

--- config.h
+++ config.h
@@ -302,10 +302,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,          movestack,              {.i = -1 } },
 
 	/*Rotate stack in place*/
-	{ MODKEY|Mod4Mask,              XK_j,          inplacerotate,          {.i = +2 } }, // same as rotatestack
-	{ MODKEY|Mod4Mask,              XK_k,          inplacerotate,          {.i = -2 } }, // same as reotatestack
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_j,          inplacerotate,          {.i = +1} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_k,          inplacerotate,          {.i = -1} },
+	/*{ MODKEY|Mod4Mask,              XK_j,          inplacerotate,          {.i = +2 } }, // same as rotatestack*/
+	/*{ MODKEY|Mod4Mask,              XK_k,          inplacerotate,          {.i = -2 } }, // same as reotatestack*/
+	{ MODKEY|Mod4Mask,              XK_j,          inplacerotate,          {.i = +1} },
+	{ MODKEY|Mod4Mask,              XK_k,          inplacerotate,          {.i = -1} },
 
 	/*Increase/decrease number of masters*/
 	{ MODKEY,                       XK_i,          incnmaster,             {.i = +1 } },
